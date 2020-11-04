995aacd250da712b8959e4225d605b27026adcff
Juan Bonforti <juan.bonforti@fluxit.com.ar>
2020-11-04 08:40:44 -0300

    Install RxSwift, RxCocoa, TableView to Home, Common struct, etc.

---
+++ TheMoviesApp/Common/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  TheMoviesApp
+//
+//  Created by Juan Bonforti on 04/11/2020.
+//
+
+import Foundation
+
+struct Constant {
+    static let apiKey = "2fc4055e5c3bd1469d8a51c9761e018a"
+    
+    struct URL {
+        static let main = "https://api.themoviedb.org/"
+        static let urlImages = "https://images.tmdb.org/t/p/w200"
+    }
+    
+    struct Endpoints {
+        static let urlListPopularMovies = "3/movie/popular"
+        static let urlDetailMovie = "3/movie"
+    }
+    
+}
--- TheMoviesApp/Home/View/HomeView.swift
+++ TheMoviesApp/Home/View/HomeView.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class HomeView: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityView: UIActivityIndicatorView!
+    
     private var router = HomeRouter()
     private var viewModel = HomeViewModel()
 
@@ -18,4 +21,16 @@ class HomeView: UIViewController {
         viewModel.bind(view: self, router: router)
     }
 
+}
+
+// MARK: - Extensions
+extension HomeView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        return cell
+    }
 }
